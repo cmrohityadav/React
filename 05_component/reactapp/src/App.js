cda0899b4912244657e4b22cf09bb769c51630b3
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-13 16:01:52 +0530

    component

---
+++ 05_component/reactapp/src/App.js
@@ -0,0 +1,11 @@
+import DogCard from './DogCard';
+
+function App() {
+  return (
+    <div>
+      <DogCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 05_component/reactapp/src/DogCard.js
@@ -0,0 +1,41 @@
+function DogCard(){
+    return(
+        //single element wrap krke send krne ka
+        <div>
+            <h2>Moti</h2>
+            <img src="https://th.bing.com/th/id/R.f961fdc43ea6c6d3b686666fec25e69d?rik=bpVfWuX%2bqbv9zg&riu=http%3a%2f%2fgora-gora-gora.com%2fwp-content%2fuploads%2f2010%2f09%2fIMG_4603a.jpg&ehk=cSH5n9arkaQXwA4FTvf46PcEdNOQidfIE8jMvXmq7ic%3d&risl=&pid=ImgRaw&r=0" alt="" />
+
+        </div>
+    );
+}
+export default DogCard;
+/*
+ return(
+        //aise nhi chalega
+        <div>
+            <h2>Moti</h2>
+            <img src="https://th.bing.com/th/id/R.f961fdc43ea6c6d3b686666fec25e69d?rik=bpVfWuX%2bqbv9zg&riu=http%3a%2f%2fgora-gora-gora.com%2fwp-content%2fuploads%2f2010%2f09%2fIMG_4603a.jpg&ehk=cSH5n9arkaQXwA4FTvf46PcEdNOQidfIE8jMvXmq7ic%3d&risl=&pid=ImgRaw&r=0" alt="" />
+
+        </div>
+        <div>
+
+        </div>
+
+    );
+*//*
+ return(
+        //aise chalega :return hum ek parent element me krte hai hamesha
+        <div>
+            <div>
+            <h2>Moti</h2>
+            <img src="https://th.bing.com/th/id/R.f961fdc43ea6c6d3b686666fec25e69d?rik=bpVfWuX%2bqbv9zg&riu=http%3a%2f%2fgora-gora-gora.com%2fwp-content%2fuploads%2f2010%2f09%2fIMG_4603a.jpg&ehk=cSH5n9arkaQXwA4FTvf46PcEdNOQidfIE8jMvXmq7ic%3d&risl=&pid=ImgRaw&r=0" alt="" />
+
+            </div>
+         <div>
+                hello
+         </div>
+        <div>
+
+    );
+*/
+
