7931e6b5c1f0f1cba49424c37f21c205fca10234
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-21 13:28:36 +0530

    Before grid

---
+++ 06_ticTacToe/tictactoe/src/App.jsx
@@ -0,0 +1,15 @@
+//import Card from "./components/card/Card"
+import './App.css'
+import Grid from "./components/Grid/Grid"
+function App() {
+  
+
+  return (
+    <>
+    <h1>rohit</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ 06_ticTacToe/tictactoe/src/components/Grid/Grid.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Card from "../card/Card";
+
+function Grid({numberOfCards}){
+
+const [board,setBoard]=useState(Array(numberOfCards).fill(""));
+
+
+return(
+
+    <div className="grid">
+
+        {board.map((el,idx) => <Card key={idx}/>)}
+
+    </div>
+);
+
+}
+export default Grid;
